d656b665d03fabe5c75beb705589204f0a60b02b
Gabriel Broilo <broilo.gabriel@gmail.com>
2016-10-21 16:29:32 +0100

    testing

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,18 +1,22 @@
 package controllers
 
 import java.net.InetSocketAddress
-import akka.actor.{Props, ActorSystem}
+
+import akka.actor.ActorSystem
+import akka.actor.Props
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.mvc._
+
 import scala.concurrent.Promise
 
 class Application extends Controller {
 
   def index = Action.async {
-    val host = "example.com"
+    val host = "localhost"
+    val port = 9021
     val promise = Promise[String]()
     val props = Props(classOf[TcpClient],
-      new InetSocketAddress(host, 80),
+      new InetSocketAddress(host, port),
       s"GET / HTTP/1.1\r\nHost: ${host}\r\nAccept: */*\r\n\r\n", promise)
 
     //Discover the actor
@@ -21,8 +25,8 @@ class Application extends Controller {
 
     //Convert the promise to Future[Result]
     promise.future map { data =>
-        tcpActor ! "close"
-        Ok(data)
+      tcpActor ! "close"
+      Ok(data)
     }
   }
 
--- app/controllers/TcpClient.scala
+++ app/controllers/TcpClient.scala
@@ -1,43 +1,47 @@
 package controllers
 
 import java.net.InetSocketAddress
+
 import akka.actor.Actor
-import akka.io.{IO, Tcp}
+import akka.io.Tcp._
+import akka.io.IO
+import akka.io.Tcp
 import akka.util.ByteString
+
 import scala.concurrent.Promise
-import Tcp._
 
 class TcpClient(remote: InetSocketAddress, requestData: String, thePromise: Promise[String]) extends Actor {
-    import context.system
 
-    println("Connecting")
-    IO(Tcp) ! Connect(remote)
+  import context.system
 
-    def receive = {
-        case CommandFailed(_: Connect) =>
-            println ("Connect failed")
-            context stop self
+  println("Connecting")
+  IO(Tcp) ! Connect(remote)
 
-        case c @ Connected(remote, local) =>
-            println ("Connect succeeded")
-            val connection = sender()
-            connection ! Register(self)
-            println("Sending request early")
-            connection ! Write(ByteString(requestData))
+  def receive = {
+    case CommandFailed(_: Connect) =>
+      println("Connect failed")
+      context stop self
 
-            context become {
-                case CommandFailed(w: Write) =>
-                    println("Failed to write request.")
-                case Received(data) =>
-                    println("Received response.")
-                    thePromise.success(data.decodeString("UTF-8"))
-                case "close" =>
-                    println("Closing connection")
-                    connection ! Close
-                case _: ConnectionClosed =>
-                    println("Connection closed by server.")
-                    context stop self
-            }
-        case _ => println("Something else is up.")
-    }
+    case c@Connected(remote, local) =>
+      println("Connect succeeded")
+      val connection = sender()
+      connection ! Register(self)
+      println("Sending request early")
+      connection ! Write(ByteString(requestData))
+
+      context become {
+        case CommandFailed(w: Write) =>
+          println("Failed to write request.")
+        case Received(data) =>
+          println("Received response.")
+          thePromise.success(data.decodeString("UTF-8"))
+        case "close" =>
+          println("Closing connection")
+          connection ! Close
+        case _: ConnectionClosed =>
+          println("Connection closed by server.")
+          context stop self
+      }
+    case _ => println("Something else is up.")
+  }
 }
